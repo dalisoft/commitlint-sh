8fa41ce024573f194f8289b77fc2c14a78442c64
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-03-25 15:39:24 +0500

    fix: [WIP] use `posix sh` way

--- presets/conventional-commits.sh
+++ presets/conventional-commits.sh
@@ -1,21 +1,18 @@
-#!/usr/bin/env bash
-set -eu
+#!/bin/sh
 
 # RegExp as variable
 regexp_commit_primary="^([a-z]+)(\(([^\)]+)\))?:\ (.+)$"
 regexp_commit_major="^([a-z]+)(\(([^\)]+)\))?!?:\ (.+)$"
-string_commit_major="BREAKING CHANGE"
+string_commit_major="BREAKING"
 
 # Release types
-RELEASE_SKIP_TYPES=("build" "chore" "docs" "test" "style" "ci" "skip ci")
-RELEASE_PATCH_TYPES=("fix" "close" "closes" "perf" "revert")
-RELEASE_MINOR_TYPES=("refactor" "feat")
-RELEASE_MAJOR_TYPES=("BREAKING CHANGE")
+RELEASE_SKIP_TYPES="build chore docs test style ci skip ci"
+RELEASE_PATCH_TYPES="fix close closes perf revert"
+RELEASE_MINOR_TYPES="refactor feat"
+RELEASE_MAJOR_TYPES="BREAKING"
 
 validate_commit() {
-  local subject="$1"
-
-  local type
+  subject="$1"
 
   # Extracting the type, scope, and description using Bash regex
   if [[ "$subject" =~ $regexp_commit_primary ]]; then
@@ -29,16 +26,16 @@ validate_commit() {
     return 1
   fi
 
-  if isValidCommitType "$type" "${RELEASE_SKIP_TYPES[@]}"; then
+  if isValidCommitType "$type" "${RELEASE_SKIP_TYPES}"; then
     echo 0
     return 0
-  elif isValidCommitType "$type" "${RELEASE_PATCH_TYPES[@]}"; then
+  elif isValidCommitType "$type" "${RELEASE_PATCH_TYPES}"; then
     echo 2
     return 2
-  elif isValidCommitType "$type" "${RELEASE_MINOR_TYPES[@]}"; then
+  elif isValidCommitType "$type" "${RELEASE_MINOR_TYPES}"; then
     echo 3
     return 3
-  elif isValidCommitType "$type" "${RELEASE_MAJOR_TYPES[@]}"; then
+  elif isValidCommitType "$type" "${RELEASE_MAJOR_TYPES}"; then
     echo 4
     return 4
   fi
--- presets/workspace.sh
+++ presets/workspace.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-set -eu
+#!/bin/sh
 
 # RegExp as variable
 regexp_commit_primary="^([a-z]+)(\(([^\)]+)\))?:\ (.+)$"
@@ -7,35 +6,32 @@ regexp_commit_major="^([a-z]+)(\(([^\)]+)\))?!?:\ (.+)$"
 string_commit_major="BREAKING CHANGE"
 
 # Release types
-RELEASE_SKIP_TYPES=("build" "chore" "docs" "test" "style" "ci" "skip ci")
-RELEASE_PATCH_TYPES=("fix" "close" "closes" "perf" "revert")
-RELEASE_MINOR_TYPES=("refactor" "feat")
-RELEASE_MAJOR_TYPES=("BREAKING CHANGE")
+RELEASE_SKIP_TYPES="build chore docs test style ci skip ci"
+RELEASE_PATCH_TYPES="fix close closes perf revert"
+RELEASE_MINOR_TYPES="refactor feat"
+RELEASE_MAJOR_TYPES="BREAKING"
 
 validate_commit() {
-  local subject="$1"
-
-  local type
-  local scope
+  subject="$1"
 
   # Extracting the type, scope, and description using Bash regex
   if [[ "$subject" =~ $regexp_commit_primary ]]; then
     type="${BASH_REMATCH[1]}"
     scope="${BASH_REMATCH[3]}"
 
-    echo "#1"
-    echo "${BASH_REMATCH[@]}"
+    # echo "#1"
+    # echo "${BASH_REMATCH[@]}"
 
   elif [[ "$subject" =~ $regexp_commit_major ]]; then
     # type="${BASH_REMATCH[1]}"
     scope="${BASH_REMATCH[3]}"
 
-    echo "#2"
-    echo "${BASH_REMATCH[@]}"
+    # echo "#2"
+    # echo "${BASH_REMATCH[@]}"
 
     type="BREAKING CHANGE"
   elif [[ "$subject" =~ $string_commit_major ]]; then
-    echo "#3"
+    # echo "#3"
 
     type="BREAKING CHANGE"
   else
@@ -52,16 +48,16 @@ validate_commit() {
     return 1
   fi
 
-  if isValidCommitType "$type" "${RELEASE_SKIP_TYPES[@]}"; then
+  if isValidCommitType "$type" "${RELEASE_SKIP_TYPES}"; then
     echo 0
     return 0
-  elif isValidCommitType "$type" "${RELEASE_PATCH_TYPES[@]}"; then
+  elif isValidCommitType "$type" "${RELEASE_PATCH_TYPES}"; then
     echo 2
     return 2
-  elif isValidCommitType "$type" "${RELEASE_MINOR_TYPES[@]}"; then
+  elif isValidCommitType "$type" "${RELEASE_MINOR_TYPES}"; then
     echo 3
     return 3
-  elif isValidCommitType "$type" "${RELEASE_MAJOR_TYPES[@]}"; then
+  elif isValidCommitType "$type" "${RELEASE_MAJOR_TYPES}"; then
     echo 4
     return 4
   fi
